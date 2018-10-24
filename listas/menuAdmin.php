565e126c0d4eb797943ce8bc6cdd832fb8aced59
Panax97 <tcpanameno@gmail.com>
2018-10-24 01:07:30 -0600

    avanze1
    arregle el menu , pero no borra nose porque

--- listas/menuAdmin.php
+++ listas/menuAdmin.php
@@ -1,4 +1,4 @@
-<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+<div id="sidebar-menu" class="main_menu_side main_menu">
                   <div class="menu_section">
                     <h3>General</h3>
                     <ul class="nav side-menu">
@@ -9,29 +9,14 @@
                            <li><a href="../listas/Permisos.php">Permisos</a></li>
                         </ul>
                       </li>
-                      <li><a><i class="fa fa-cog"></i> Categoria <span class="fa fa-chevron-down"></span></a>
+                      <li><a><i class="fa fa-cog"></i> Mantenimientos <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
                           <li><a href="../listas/Categorias.php">Categorias</a></li>
-                           <li><a href="../listas/TipoUsuario.php">Materiales</a></li>
-                           
+                           <li><a href="../listas/Bodega.php">Bodega</a></li> 
+                           <li><a href="../listas/naves.php">Naves</a></li>                          
                         </ul>
                       </li>
-                        <li><a><i class="fa fa-cog"></i> Bodegas <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                          <li><a href="../listas/Bodega.php">Bodega</a></li>
-                           <li><a href="../listas/TipoUsuario.php">Materiales</a></li>
-                           
-                        </ul>
-                      </li>
-                    <li><a><i class="fa fa-cog"></i> Materiales <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                          <li><a href="../listas/Categorias.php">Categoria</a></li>
-                           <li><a href="../listas/TipoUsuario.php">Materiales</a></li>
-                           
-                        </ul>
-                      </li>
+                    
                     </ul>
                   </div>
-                  
-    
                 </div>
